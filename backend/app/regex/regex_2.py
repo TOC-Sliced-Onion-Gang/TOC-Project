49256ba317d7c27b0f8393b968cdf89b8a1b54cd
Icarus <63220735+shoguncoffee@users.noreply.github.com>
2024-09-18 22:38:07 +0700

    tiny bug, commit anyway

--- backend/app/regex/regex_2.py
+++ backend/app/regex/regex_2.py
@@ -1,8 +1,41 @@
 import re
-from flask import Blueprint
+import requests
+from flask import Blueprint, request
 
+DOMAIN = 'https://pypi.org'
 router = Blueprint('regex_2', __name__)
 
-@router.route('/')
-def library():
-    return "Hello World!"
+@router.route('/search')
+    
+def search():#(query: str, page: int = 1):
+    query = request.args.get('q') or ''
+    page = request.args.get('page')
+    with requests.Session() as session:
+        search_html = session.get(f'{DOMAIN}/search/', 
+            params={
+                'q': query,
+                'page': page
+            }
+        ).text
+        lst = []
+        
+        for path in re.findall('"package-snippet" href="([^"]+)"', search_html):
+            lib_html = session.get(f'{DOMAIN}{path}').text
+            
+            name = re.search(r'header__name">\s*(\w+)', lib_html, re.DOTALL)
+            author = re.search(r'Author:.*?"(.*?)">\s*(.*?)\s*<', lib_html, re.DOTALL)
+            action = re.search(r'description__summary">\s*(.*?)\s*<', lib_html, re.DOTALL)
+            description = re.search(r'<div class="project-description">.*?</div>', lib_html, re.DOTALL)
+            
+            lst.append({
+                'name': name and name.group(1),
+                'author': 
+                    author and {
+                        'addr': author.group(1),
+                        'name': author.group(2),
+                    },
+                'action': action and action.group(1),  
+                'description': None # description and description.group(),
+            })
+        
+        return lst
