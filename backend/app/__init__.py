dd1bc057a9cae4c13a60983e22448b92a6c3c948
Icarus <63220735+shoguncoffee@users.noreply.github.com>
2024-09-18 22:50:02 +0700

    conflict, my fault, sorry to modifly

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
-from app.regex import regex_2
+from app.routes import router
 
 app = Flask(__name__)
 CORS(app)
-
-
-from app.routes import router
 
 app.register_blueprint(router)
