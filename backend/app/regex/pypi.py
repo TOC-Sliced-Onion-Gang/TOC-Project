79b288e4be9168aff0e936f270fa14fc1f154eb0
Icarus <63220735+shoguncoffee@users.noreply.github.com>
2024-09-19 09:55:43 +0700

    add my route & update pypi

--- backend/app/regex/pypi.py
+++ backend/app/regex/pypi.py
@@ -1,15 +1,10 @@
 import re
 import requests
-from flask import Blueprint, request
 
 DOMAIN = 'https://pypi.org'
-router = Blueprint('regex_2', __name__)
 
-@router.route('/search')
-    
-def search():#(query: str, page: int = 1):
-    query = request.args.get('q') or ''
-    page = request.args.get('page')
+
+def search(query: str, page: int = 1):    
     with requests.Session() as session:
         search_html = session.get(f'{DOMAIN}/search/', 
             params={
@@ -17,17 +12,16 @@ def search():#(query: str, page: int = 1):
                 'page': page
             }
         ).text
-        lst = []
         
-        for path in re.findall('"package-snippet" href="([^"]+)"', search_html):
+        for path in re.findall(r'"package-snippet" href="(.*?)"', search_html):
             lib_html = session.get(f'{DOMAIN}{path}').text
             
-            name = re.search(r'header__name">\s*(\w+)', lib_html, re.DOTALL)
+            name = re.search(r'header__name">\s*([\w-]+)', lib_html, re.DOTALL)
             author = re.search(r'Author:.*?"(.*?)">\s*(.*?)\s*<', lib_html, re.DOTALL)
             action = re.search(r'description__summary">\s*(.*?)\s*<', lib_html, re.DOTALL)
             description = re.search(r'<div class="project-description">.*?</div>', lib_html, re.DOTALL)
             
-            lst.append({
+            yield {
                 'name': name and name.group(1),
                 'author': 
                     author and {
@@ -36,6 +30,4 @@ def search():#(query: str, page: int = 1):
                     },
                 'action': action and action.group(1),  
                 'description': None # description and description.group(),
-            })
-        
-        return lst
+            }
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
-
+from flask import Blueprint, request
+from app.regex import pypi
+from app.regex.python_org import sho_section
 
 router = Blueprint('router', __name__)
 
@@ -8,4 +9,13 @@ router = Blueprint('router', __name__)
 def member():
     return {"members": ["Member1", "Member2", "Member3"]}
 
+@router.route('/pypi/search') # อาจจะ > 5 วิ
+def pypi_search(): 
+    query = request.args.get('q') or ''
+    page = int(request.args.get('page') or 0)
+    
+    return list(pypi.search(query, page))
 
+@router.route('/shogun')
+def shogun():
+    return list(sho_section.get())
