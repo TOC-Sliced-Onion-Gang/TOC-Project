65794900b642eb87c585d22667070907d26e2992
Icarus <63220735+shoguncoffee@users.noreply.github.com>
2024-09-19 09:53:44 +0700

    my implement for python.org

---
+++ backend/app/regex/python_org/sho_section.py
@@ -0,0 +1,27 @@
+import re
+import requests
+
+DOMAIN = 'https://docs.python.org/3.12/library/'
+my_title = [
+    'Data Compression and Archiving',
+    'Internet Protocols and Support',
+    'Python Runtime Services',
+]
+
+def get():
+    html = requests.get(DOMAIN).text
+
+    for title in my_title:
+        match = re.search(rf'href="(.*?)">{title}', html)
+        assert match
+        
+        path = match.group(1)
+        page = requests.get(f'{DOMAIN}{path}').text
+
+        for lib_path in re.findall(r'toctree-l1.*?href="(.*?)"', page):
+            lib_html = requests.get(f'{DOMAIN}{lib_path}').text
+            
+            name = re.search(r'<title>(\w+)', lib_html)
+            assert name
+
+            yield name.group(1)
