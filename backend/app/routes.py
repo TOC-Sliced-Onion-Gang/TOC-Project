5f1c278933940690deb39142474a8ddf15f2ff8e
K_firm1208 <106590971+Kfirm1208@users.noreply.github.com>
2024-09-26 00:16:40 +0700

    add firm() get method and get data successfully

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request
+from flask import Blueprint, request ,jsonify
 from app.regex import pypi
 from app.regex.python_org import sho_section
+from app.regex.python_org.firm_section import first_level,second_level,sections
 
 router = Blueprint('router', __name__)
 
@@ -19,3 +20,17 @@ def pypi_search():
 @router.route('/shogun')
 def shogun():
     return list(sho_section.get()) # name list
+
+@router.route('/firm',methods=['GET'])
+def firm():
+    # Check for a query parameter to perform a search
+    all_libraries = []
+    for section_name in sections.keys():
+        first_level_data = first_level(section_name)
+        if 'href' in first_level_data:
+            href = first_level_data['href']
+            libraries = second_level(href)
+            if isinstance(libraries, list):
+                all_libraries.extend(libraries) 
+
+    return jsonify(all_libraries)
